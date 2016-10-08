5b743ffbe112c70fe012e4d4452bbd0772b3bdff
jin032 <jin032@yahoo.com>
2016-10-08 19:25:03 +0500

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+	acts_as_votable
+	belongs_to :user
+	has_many :comments
+
+	has_attached_file :image, styles: { medium: "300x300>", small: "200x150>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
